d911f3a83993c91fb4854b9fd5994efe61ece7c6
Wouter de Vos <wouter@codaisseur.com>
2016-02-24 22:07:46 +0100

    Production settings

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -25,10 +25,19 @@ module.exports = {
     })
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src')
-    }]
+    loaders: [
+      {
+        test: /\.jsx?/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      },
+      { test: /\.woff$/,   loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.woff2$/,  loader: "url-loader?limit=10000&minetype=application/font-woff" },
+      { test: /\.ttf$/,    loader: "file-loader" },
+      { test: /\.eot$/,    loader: "file-loader" },
+      { test: /\.svg$/,    loader: "file-loader" },
+      { test: /\.png$/,    loader: "file-loader" },
+      { test: /\.scss$/,   loader: 'style!css!sass'}
+    ]
   }
 };
